a47e4452c5cf48dc0e7c7db33c669ad0fff8bb02
Cyprian  Nyakundi <cn@Cyprians-MacBook-Pro.local>
2020-08-24 13:53:26 +0300

    fix date

--- js/scripts.js
+++ js/scripts.js
@@ -1,49 +1,43 @@
-//Using Jquery for the Akan names backend logic
+function dayOfTheWeek(century, year, day, month) {
+  var date = new Date();
+  var n = date.getDay()
+  return dayOfTheWeek
+}
 
-var akanNames = function (year, month, day, gender) {
-  // Akan name logic showing that we need yr,month,day and gender for the akan name functions
-
-  var femaleNamesArray = [
-    "Akosua",
-    "Adwoa",
-    "Abenaa",
-    "Akua",
-    "Yaa",
-    "Afua",
-    "Ama",
-  ]; // Female names Array
-  var maleNamesArray = [
-    "Kwasi",
-    "Kudwo",
-    "Kwabena",
-    "Kwaku",
-    "Yaw",
-    "Kofi",
-    "Kwame",
-  ]; //Male names Array
-  var day = new Date(year, month, day); // Introduce Date object to work with date & time including days, months, years
-  if (gender === "maleNamesArray") {
-    return day && maleNamesArray[day.getDay()]; //Looping with if else conditional statements to compare days and names arrays
+function getname(day, gender) {
+  if (gender == "male") {
+    switch (day) {
+      case 1:
+        return "Kwasi";
+    }
   } else {
-    return day && femaleNamesArray[day.getDay()];
+    switch (day) {
+      case 1:
+        return "Akosua";
+    }
   }
-};
-
-//________________________________________________________________________________________________________
-
-//Introducing the front end logic by introducing the $( document ).ready() block
-
-$(document).ready(function () {
-  // Line one- We use  the $ convention instead of Jquery
-  $("form#form").submit(function (event) {
-    event.preventDefault();
-    var year = parseInt($("#year").val());
-    var month = parseInt($("#month").val());
-    var day = parseInt($("#date").val());
-    var gender = $("input:radio[name=gender]:checked").val();
-    var result = akanNames(year, month, day, gender);
-    alert("Your akan name is: " + result);
-    //refresh page
-    document.getElementById("form").reset();
-  });
-});
+}
+function inputs(date, month, year, gender) {
+  if (date > 31 || date < 1) {
+    alert("Please enter a valid date");
+    return;
+  }
+  if (month > 12 || month < 1) {
+    alert("Please enter a valid month");
+    return;
+  }
+  if (year < 999) {
+    alert("Please enter a valid year");
+    return;
+  }
+  if (gender != "male" && gender != "female") {
+    console.log(gender);
+    alert("Please enter a valid gender");
+    return;
+  }
+  console.log("inputs are valid");
+  var century = year.substring(0, 2);
+  var day = dayOfTheWeek(century, year, date, month);
+  var name = getname(day, gender);
+  alert("Your name is " + name);
+}
